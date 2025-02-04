db70ba51c3123313c498d5e8235587f04ff9c484
Tạ Tuấn Anh <ovftank@gmail.com>
2025-02-04 07:45:23 +0700

    chore: init backend structure

---
+++ src/main/java/com/ovftank/image/ovftank/OvftankApplication.java
@@ -0,0 +1,13 @@
+package com.ovftank.image.ovftank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvftankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvftankApplication.class, args);
+	}
+
+}
--- src/pages/gallery.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Gallery: React.FC = () => {
-	return <></>;
-};
-
-export default Gallery;
